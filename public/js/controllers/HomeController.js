f549587be740e84b2995d1a6ba6e2a3c7d1c6bf7
ggarri <gabriel.garrido.work@gmail.com>
2018-12-25 12:54:02 +0100

    Adding intervals for refreshing block, txs and stats

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -1,7 +1,19 @@
-angular.module('BlocksApp').controller('HomeController', function($rootScope, $scope, $http, $timeout) {
+angular.module('BlocksApp')
+  .controller('HomeController', function($rootScope, $scope, $http, $timeout) {
     $scope.$on('$viewContentLoaded', function() {
-        // initialize core components
-        App.initAjax();
+      // initialize core components
+      App.initAjax();
+
+      $scope.refreshBlocksInterval = setInterval(function() {
+        console.log("Refresh blocks and transactions");
+        $scope.reloadBlocks();
+        $scope.reloadTransactions();
+      }, 5000);
+    });
+
+    $scope.$on('$destroy', function() {
+      clearInterval($scope.refreshBlocksInterval);
+      clearInterval($scope.refeshStatsInterval);
     });
 
     var URL = '/data';
@@ -13,47 +25,59 @@ angular.module('BlocksApp').controller('HomeController', function($rootScope, $s
       $http({
         method: 'POST',
         url: URL,
-        data: {"action": "latest_blocks"}
+        data: { "action": "latest_blocks" }
       }).then(function(resp) {
         $scope.latest_blocks = resp.data.blocks;
         $scope.blockLoading = false;
       });
     }
+
     $scope.reloadTransactions = function() {
       $scope.txLoading = true;
       $http({
         method: 'POST',
         url: URL,
-        data: {"action": "latest_txs"}
+        data: { "action": "latest_txs" }
       }).then(function(resp) {
         $scope.latest_txs = resp.data.txs;
         $scope.txLoading = false;
       });
     }
+
     $scope.reloadBlocks();
     $scope.reloadTransactions();
     $scope.txLoading = false;
     $scope.blockLoading = false;
     $scope.settings = $rootScope.setup;
-})
-.directive('simpleSummaryStats', function($http) {
-  return {
-    restrict: 'E',
-    templateUrl: '/views/summary-stats.html',
-    scope: true,
-    link: function(scope, elem, attrs){
-      scope.stats = {};
-      scope.stats.onlinevalidators = 1;
-      scope.stats.totalvalidators = 1;
-      scope.stats.usdPht = 0.15;
-      var statsURL = "/web3relay";
-      $http.post(statsURL, {"action": "hashrate"})
-       .then(function(res){
-          scope.stats.hashrate = res.data.hashrate;
-          scope.stats.difficulty = res.data.difficulty;
-          scope.stats.blockHeight = res.data.blockHeight;
-          scope.stats.blockTime = res.data.blockTime;
-	    });
+  })
+  .directive('simpleSummaryStats', function($http) {
+    return {
+      restrict: 'E',
+      templateUrl: '/views/summary-stats.html',
+      scope: true,
+      link: function(scope, elem, attrs) {
+        scope.stats = {};
+        scope.stats.onlinevalidators = 1;
+        scope.stats.totalvalidators = 1;
+        scope.stats.usdPht = 0.15;
+
+        scope.refreshStats = function() {
+          $http.post("/web3relay", { "action": "hashrate" })
+            .then(function(res) {
+              console.log(res.data);
+              scope.stats.hashrate = res.data.hashrate;
+              scope.stats.difficulty = res.data.difficulty;
+              scope.stats.blockHeight = res.data.blockHeight;
+              scope.stats.blockTime = res.data.blockTime;
+            });
+        }
+
+        scope.refreshStats();
+
+        scope.$parent.refeshStatsInterval = setInterval(function() {
+          console.log("Refresh stats");
+          scope.refreshStats();
+        }, 5000);
 
         // $http.post(etcEthURL, { "action": "etceth" })
         //     .then(function(res) {
@@ -64,12 +88,12 @@ angular.module('BlocksApp').controller('HomeController', function($rootScope, $s
         //         scope.stats.etcDiff = res.data.etcDiff;
         //         scope.stats.etcEthDiff = res.data.etcEthDiff;
         //     });
-  }
-  }
-})
-.directive('siteNotes', function() {
-  return {
-    restrict: 'E',
-    templateUrl: '/views/site-notes.html'
-  }
-});
+      }
+    }
+  })
+  .directive('siteNotes', function() {
+    return {
+      restrict: 'E',
+      templateUrl: '/views/site-notes.html'
+    }
+  });
--- routes/stats.js
+++ routes/stats.js
@@ -26,8 +26,8 @@ module.exports = function(req, res) {
   if (!("action" in req.body))
     res.status(400).send();
   
-  else if (req.body.action=="miners") 
-    getMinerStats(req, res)
+  else if (req.body.action=="validators")
+    getValidatorsStats(req, res)
   
   else if (req.body.action=="hashrate") 
     getHashrate(res);
@@ -37,10 +37,10 @@ module.exports = function(req, res) {
   
 }
 /**
-  Aggregate miner stats
+  Aggregate validators stats
 **/
-var getMinerStats = function(req, res) {
-  var range =  6*60*60; // 6 hours
+var getValidatorsStats = function(req, res) {
+  var range =  6*60; // 1 hours
   // check validity of range
   if (req.body.range && req.body.range < 60 * 60 * 24 * 7) {
     range = parseInt(req.body.range);
@@ -59,7 +59,7 @@ var getMinerStats = function(req, res) {
       return;
     }
     var blockNumber = docs[0].number;
-    console.log('getMinerStats(): blockNumber = ' + blockNumber);
+    console.log('getValidatorsStats(): blockNumber = ' + blockNumber);
     Block.aggregate([
         { $match: { number: { $gte: blockNumber } } },
         { $group: {
@@ -176,65 +176,5 @@ var getHashrate = function(res) {
         "difficulty": docs[0].difficulty
     }));
     res.end();
-  });
-}
-/**
-  OLD CODE DON'T USE
-  Swipe ETC ETH data
-**/
-var getEtcEth = function(res) {
-  var options = [{
-    host: 'api.minergate.com',
-    path: '/1.0/etc/status',
-    method: 'GET',
-    data: 'etc'
-  },{
-    host: 'api.minergate.com',
-    path: '/1.0/eth/status',
-    method: 'GET',
-    data: 'eth'
-  }];
-  
-  async.map(options, function(opt, callback) {
-    
-    https.request(opt, function(mg) {
-      mg.on('data', function (data) {
-        try {
-          var result = JSON.parse(data.toString());
-          result.chain = opt.data;
-          callback(null, result);
-        } catch (e) {
-          callback(e);
-        }
-      })
-    }).end();
-  }, function(err, results) {
-    if (err) {
-      console.error(err);
-      res.status(500).send();
-    } else {
-      if (results.length < 2)
-        res.status(500).send();
-      else {
-        var c = ((results[0].chain == "etc") ? 0 : 1);
-        var h = 1 - c;
-        var etcHashrate = parseInt(results[c].instantHashrate);
-        var ethHashrate = parseInt(results[h].instantHashrate);
-        var etcDiff = results[c].difficulty.toFixed(2);
-        var ethDiff = results[h].difficulty.toFixed(2);
-        var etcEthHash = parseInt(100*etcHashrate/ethHashrate);
-        var etcEthDiff = parseInt(100*etcDiff/ethDiff);
-        res.write(JSON.stringify({
-          "etcHashrate": etcHashrate,
-          "ethHashrate": ethHashrate,
-          "etcDiff": etcDiff,
-          "ethDiff": ethDiff,
-          "etcEthHash": etcEthHash,
-          "etcEthDiff": etcEthDiff
-        }));
-        res.end();
-      } 
-    }
-
   });
 }
